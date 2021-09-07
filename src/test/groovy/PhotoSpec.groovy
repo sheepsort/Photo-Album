b3e098c58bda3592903c19e06b15dab26bf400ed
Jim <jim@sheepsort.com>
2021-09-07 16:49:32 -0500

    Refactor photo spec for reusability

--- src/test/groovy/PhotoSpec.groovy
+++ src/test/groovy/PhotoSpec.groovy
@@ -1,19 +1,23 @@
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class PhotoSpec extends Specification {
-  void 'A Photo will instantiate when given valid parameters'() {
-    given:
-    // https://jsonplaceholder.typicode.com/photos?id=1
-    Map photoParams = [
+  Map photoParams
+
+  void beforeEach() {
+    // example photo from https://jsonplaceholder.typicode.com/photos?id=1
+    photoParams = [
         "albumId"     : 1,
         "id"          : 1,
         "title"       : "accusamus beatae ad facilis cum similique qui sunt",
         "url"         : "https://via.placeholder.com/600/92c952",
         "thumbnailUrl": "https://via.placeholder.com/150/92c952"
     ]
+  }
 
+  void 'A Photo will instantiate when given valid parameters'() {
     when:
-    Photo photo = new Photo(photoParams)
+    new Photo(photoParams)
 
     then:
     notThrown()
