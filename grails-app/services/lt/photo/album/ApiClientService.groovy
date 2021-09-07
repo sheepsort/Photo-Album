1ff893d679deb2bd359d2160197ef7368e3a26b7
Jim <jim@sheepsort.com>
2021-09-07 17:11:25 -0500

    Initialize a service to fetch data from the provided endpoint

---
+++ grails-app/services/lt/photo/album/ApiClientService.groovy
@@ -0,0 +1,11 @@
+package lt.photo.album
+
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class ApiClientService {
+
+    def serviceMethod() {
+
+    }
+}
---
+++ src/test/groovy/lt/photo/album/ApiClientServiceSpec.groovy
@@ -0,0 +1,18 @@
+package lt.photo.album
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class ApiClientServiceSpec extends Specification implements ServiceUnitTest<ApiClientService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
