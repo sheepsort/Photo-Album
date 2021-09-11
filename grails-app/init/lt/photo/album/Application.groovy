7369a0d4431420d0c92432703864c1c13a11ce5a
Jim <jim@sheepsort.com>
2021-09-10 23:52:16 -0500

    launch the photo service's logging method on app start

--- grails-app/init/lt/photo/album/Application.groovy
+++ grails-app/init/lt/photo/album/Application.groovy
@@ -2,9 +2,12 @@ package lt.photo.album
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import grails.util.Holders
 
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+        // kick off the operation normally exposed via RESTful endpoint in main() for simplicity's sake
+        Holders.grailsApplication.mainContext.photoService.printPhotosByAlbum()
     }
 }
